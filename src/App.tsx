75ffd6958cab8cf24fc080950fed402a286b0809
Luis Eugenio SC <luis.saldarriaga@hass-colombia.com>
2023-02-06 16:53:12 -0500

    Se añáden los proveedores y toastify

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,14 @@
+import Router from "config/routes";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <>
+      <Router />
+      <ToastContainer />
+    </>
+  );
 }
 
 export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,30 @@
+import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
+import { createTheme, ThemeProvider } from "@mui/material";
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+
 import App from "./App";
 import "./styles/base.css";
-
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#fc466b",
+    },
+  },
+});
+const client = new ApolloClient({
+  uri: "http://localhost:3000/gql",
+  cache: new InMemoryCache(),
+});
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <ApolloProvider client={client}>
+      <ThemeProvider theme={theme}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </ThemeProvider>
+    </ApolloProvider>
   </React.StrictMode>
 );
