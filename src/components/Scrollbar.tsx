283e19425745107b933296ac75ce34a9cd46c80b
Luis Eugenio SC <luis.saldarriaga@hass-colombia.com>
2023-02-08 18:51:15 -0500

    Se cambian estilos y se añaden dos componentes nuevos

---
+++ src/components/Scrollbar.tsx
@@ -0,0 +1,66 @@
+import { Box } from "@mui/material";
+// material
+import { alpha, styled } from "@mui/material/styles";
+import SimpleBarReact from "simplebar-react";
+
+// ----------------------------------------------------------------------
+
+const RootStyle = styled("div")({
+  flexGrow: 1,
+  height: "100%",
+  overflow: "hidden",
+});
+
+const SimpleBarStyle = styled(SimpleBarReact)(({ theme }) => ({
+  maxHeight: "100%",
+  "& .simplebar-scrollbar": {
+    "&:before": {
+      backgroundColor: alpha(theme.palette.grey[600], 0.48),
+    },
+    "&.simplebar-visible:before": {
+      opacity: 1,
+    },
+  },
+  "& .simplebar-track.simplebar-vertical": {
+    width: 10,
+  },
+  "& .simplebar-track.simplebar-horizontal .simplebar-scrollbar": {
+    height: 6,
+  },
+  "& .simplebar-mask": {
+    zIndex: "inherit",
+  },
+}));
+
+// ----------------------------------------------------------------------
+
+type ScrollbarProps = {
+  children: React.ReactNode;
+  sx?: any;
+  className?: any;
+};
+
+const Scrollbar = ({ children, sx = {}, ...other }: ScrollbarProps) => {
+  const isMobile =
+    /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
+      navigator.userAgent
+    );
+
+  if (isMobile) {
+    return (
+      <Box sx={{ overflowX: "auto", ...sx }} {...other}>
+        {children}
+      </Box>
+    );
+  }
+
+  return (
+    <RootStyle>
+      <SimpleBarStyle clickOnTrack={false} sx={sx} {...other}>
+        {children}
+      </SimpleBarStyle>
+    </RootStyle>
+  );
+};
+
+export default Scrollbar;
