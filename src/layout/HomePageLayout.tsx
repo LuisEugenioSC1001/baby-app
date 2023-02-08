1182c303656cecffb9e012ba7c03a5f037e76dcf
Luis Eugenio SC <luis.saldarriaga@hass-colombia.com>
2023-02-08 18:51:30 -0500

    Se cambian los estilos de la página principal

--- src/layout/HomePageLayout.tsx
+++ src/layout/HomePageLayout.tsx
@@ -6,7 +6,7 @@ const HomePageLayout = () => {
   return (
     <div className="w-screen h-screen bg-watercolor object-fill">
       <div className="flex flex-col items-center justify-center h-screen">
-        <div className="bg-white md:w-[70%] md:h-[70%] w-[90%] h-[70%]  rounded-xl flex flex-col justify-center px-4">
+        <div className="bg-white md:w-[70%] md:h-[70%] w-[90%] h-[90%]  rounded-xl flex flex-col justify-center px-4">
           <motion.div
             key={pathname}
             initial={{ opacity: 0 }}
